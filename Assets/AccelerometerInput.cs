cfe5bede89348f431e2c39022cf24458093941a8
Reynaldo Vargas <rdv230@nyu.edu>
2016-02-24 12:43:04 -0500

    First Commit, added project file with slider prototype.

---
+++ Assets/AccelerometerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+
+	void Update() {
+		
+
+		
+
+
+		Vector3 acceleration = Vector3.zero;
+		for (int i = 0; i < Input.accelerationEventCount; i++) {
+			AccelerationEvent accEvent = Input.GetAccelerationEvent(i);
+			acceleration += accEvent.acceleration ;
+		}
+		print(acceleration);
+	}
+}
---
+++ Assets/SoundController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour {
+
+	public AudioClip MajorA;
+	public AudioClip MajorB;
+	public AudioClip MajorC;
+	public AudioClip MajorD;
+	public AudioClip MajorE;
+	public AudioClip MajorF;
+	public AudioClip MajorG;
+
+
+	public AudioSource[] audioSources;
+	bool isPlaying;
+
+	public Slider sliderObj;
+	public float currentSliderVal;
+
+
+	// Use this for initialization
+	void Start () {
+		currentSliderVal = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount >= 1){
+			Debug.Log("is");
+		if(Input.GetTouch(0).phase == TouchPhase.Ended){
+				Debug.Log("It as worked + \n currentSliderVal: "+ currentSliderVal + "       sliderObj.value: " + sliderObj.value);
+				Debug.Log("Yes");
+
+				ChangeClip(sliderObj.value);
+				Debug.Log("CHANGE");
+			}
+		}
+	}
+
+	public void ChangeClip(float i){
+
+		int num = Mathf.RoundToInt(i);
+
+		audioSources[num].Play();
+		Debug.Log(audioSources[num]);
+	}
+
+	IEnumerator Wait(){
+		yield return new WaitForSeconds(5);
+	}
+}
