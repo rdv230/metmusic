aba8b0ae4b412f9fbb36befec51958443d38fa10
Sweta Mohapatra <s.moha526@gmail.com>
2016-02-25 14:12:28 -0500

    Adding PhoneGap accelerometer test app
    v1 of pulling phone’s accelerometer values via PhoneGap

---
+++ accel-test/platforms/ios/www/js/index.js
@@ -0,0 +1,18 @@
+
+window.addEventListener("load", function load() {
+    console.log("Loaded");
+    window.addEventListener('devicemotion', onDeviceMotion, false);
+
+});
+
+function onDeviceMotion(e)
+{
+    var x = e.acceleration.x;
+    var y = e.acceleration.y;
+    var z = e.acceleration.z;
+
+   
+   document.getElementById('x').innerText = (x*100);
+   document.getElementById('y').innerText = (y*100);
+   document.getElementById('z').innerText = (z*100);
+}
