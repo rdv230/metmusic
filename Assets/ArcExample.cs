57f1da1e13d4da51c715730ec53189c894ca38b1
Reynaldo Vargas <rdv230@nyu.edu>
2016-02-28 16:20:43 -0500

    Added "Swipe" Functionality.

--- Assets/ArcExample.cs
+++ Assets/ArcExample.cs
@@ -7,8 +7,15 @@ public class ArcExample : MonoBehaviour {
 	private AudioSource audio;
 	public AudioClip[] chords;
 
+	public bool hasTouched;
+	public bool swipeToGo;
+	public bool playSwipe;
 	void Start () {
 
+
+		hasTouched = false;
+		swipeToGo = false;
+		playSwipe = false;
 		audio = GetComponent<AudioSource>();
 
 
@@ -26,9 +33,30 @@ public class ArcExample : MonoBehaviour {
 
 		Debug.Log (transform.rotation);
 
+		//Detects Touch and changes a bool to activate notes depending on the rotation of that cube we have.
+
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+			hasTouched = true;
+		} else{
+			hasTouched = false;
+		}
+
+		if(swipeToGo){
+			if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
+				swipeToGo = false;
+			}
+		}
+
+		if(Input.touchCount > 0 && !swipeToGo && Input.GetTouch(0).phase == TouchPhase.Stationary){
+			swipeToGo = true;
+			playSwipe = true;
+		}
+			
+
+
 		if (transform.rotation.z <= 1 && transform.rotation.z > 0.7)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched||playSwipe)
 			{
 				audio.PlayOneShot(chords[0]);
 			}
@@ -36,50 +64,59 @@ public class ArcExample : MonoBehaviour {
 
 		if (transform.rotation.z <= 0.7 && transform.rotation.z > 0.4)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched|| playSwipe)
 			{
 				audio.PlayOneShot(chords[1]);
+				playSwipe = false;
 			}
 		}
 
 		if (transform.rotation.z <= 0.4 && transform.rotation.z > 0.1)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched|| playSwipe)
 			{
 				audio.PlayOneShot(chords[2]);
+				playSwipe = false;
 			}
 		}
 
 		if (transform.rotation.z <= 0.1 && transform.rotation.z > -0.2)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched|| playSwipe)
 			{
 				audio.PlayOneShot(chords[3]);
+				playSwipe = false;
 			}
 		}
 
 		if (transform.rotation.z <= -0.2 && transform.rotation.z > -0.5)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched|| playSwipe)
 			{
 				audio.PlayOneShot(chords[4]);
+				playSwipe = false;
 			}
 		}
 
 		if (transform.rotation.z <= -0.5 && transform.rotation.z > -0.8)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched|| playSwipe)
 			{
 				audio.PlayOneShot(chords[5]);
+				playSwipe = false;
 			}
 		}
 
 		if (transform.rotation.z <= -0.8 && transform.rotation.z > -1)
 		{
-			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			if (hasTouched)
 			{
 				audio.PlayOneShot(chords[6]);
+				playSwipe = false;
+			
 			}
 		}
 	}
+
+
 }
