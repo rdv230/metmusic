95cc1fdf5e911a24dbfd10a712588be2f2a7e0f2
Reynaldo Vargas <rdv230@nyu.edu>
2016-02-28 15:34:39 -0500

    Added Carl's prototype into our project.

---
+++ Assets/ArcExample.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcExample : MonoBehaviour {
+
+	Gyroscope gyro;
+	private AudioSource audio;
+	public AudioClip[] chords;
+
+	void Start () {
+
+		audio = GetComponent<AudioSource>();
+
+
+		gyro = Input.gyro;
+
+		if(!gyro.enabled)
+		{
+			gyro.enabled = true;
+		}
+	}
+
+	void Update () 
+	{
+		gameObject.transform.rotation  = gyro.attitude;
+
+		Debug.Log (transform.rotation);
+
+		if (transform.rotation.z <= 1 && transform.rotation.z > 0.7)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[0]);
+			}
+		}
+
+		if (transform.rotation.z <= 0.7 && transform.rotation.z > 0.4)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[1]);
+			}
+		}
+
+		if (transform.rotation.z <= 0.4 && transform.rotation.z > 0.1)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[2]);
+			}
+		}
+
+		if (transform.rotation.z <= 0.1 && transform.rotation.z > -0.2)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[3]);
+			}
+		}
+
+		if (transform.rotation.z <= -0.2 && transform.rotation.z > -0.5)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[4]);
+			}
+		}
+
+		if (transform.rotation.z <= -0.5 && transform.rotation.z > -0.8)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[5]);
+			}
+		}
+
+		if (transform.rotation.z <= -0.8 && transform.rotation.z > -1)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+			{
+				audio.PlayOneShot(chords[6]);
+			}
+		}
+	}
+}
---
+++ Assets/ExampleGyro.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleGyro : MonoBehaviour {
+
+	public float shakeSpeed;
+	public AudioClip[] chords;
+	public GameObject[] letters;
+	private AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+
+	void Update() {
+		
+
+			if (Input.deviceOrientation == DeviceOrientation.FaceDown)
+			{
+			
+				letters[0].gameObject.SetActive(true);
+				letters[1].gameObject.SetActive(false);
+				letters[2].gameObject.SetActive(false);
+				letters[3].gameObject.SetActive(false);
+				letters[4].gameObject.SetActive(false);
+				letters[5].gameObject.SetActive(false);
+				letters[6].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[0]);
+				}
+
+			}
+
+			if (Input.deviceOrientation == DeviceOrientation.FaceUp)
+			{
+			
+				letters[1].gameObject.SetActive(true);
+				letters[6].gameObject.SetActive(false);
+				letters[2].gameObject.SetActive(false);
+				letters[3].gameObject.SetActive(false);
+				letters[4].gameObject.SetActive(false);
+				letters[5].gameObject.SetActive(false);
+				letters[0].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[1]);
+				}
+
+			}
+
+			if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+			{
+			
+				letters[2].gameObject.SetActive(true);
+				letters[1].gameObject.SetActive(false);
+				letters[6].gameObject.SetActive(false);
+				letters[3].gameObject.SetActive(false);
+				letters[4].gameObject.SetActive(false);
+				letters[5].gameObject.SetActive(false);
+				letters[0].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[2]);
+				}
+
+			}
+
+			if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+			{
+			
+				letters[3].gameObject.SetActive(true);
+				letters[1].gameObject.SetActive(false);
+				letters[2].gameObject.SetActive(false);
+				letters[6].gameObject.SetActive(false);
+				letters[4].gameObject.SetActive(false);
+				letters[5].gameObject.SetActive(false);
+				letters[0].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[3]);
+				}
+
+			}
+
+			if (Input.deviceOrientation == DeviceOrientation.Portrait)
+			{
+			
+				letters[4].gameObject.SetActive(true);
+				letters[1].gameObject.SetActive(false);
+				letters[2].gameObject.SetActive(false);
+				letters[3].gameObject.SetActive(false);
+				letters[6].gameObject.SetActive(false);
+				letters[5].gameObject.SetActive(false);
+				letters[0].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[4]);
+				}
+			}
+
+			if (Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
+			{
+
+				letters[5].gameObject.SetActive(true);
+				letters[1].gameObject.SetActive(false);
+				letters[2].gameObject.SetActive(false);
+				letters[3].gameObject.SetActive(false);
+				letters[4].gameObject.SetActive(false);
+				letters[6].gameObject.SetActive(false);
+				letters[0].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[5]);
+				}
+
+			}
+
+			else if (Input.deviceOrientation == DeviceOrientation.Unknown)
+			{
+
+				letters[6].gameObject.SetActive(true);
+				letters[1].gameObject.SetActive(false);
+				letters[2].gameObject.SetActive(false);
+				letters[3].gameObject.SetActive(false);
+				letters[4].gameObject.SetActive(false);
+				letters[5].gameObject.SetActive(false);
+				letters[0].gameObject.SetActive(false);
+
+				if (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 0)
+				{
+					audio.PlayOneShot(chords[6]);
+				}
+
+			}
+
+
+		
+	}
+}
