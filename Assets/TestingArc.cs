a9ec426357d2ea95c4a762151aaf3c56b4632404
Reynaldo Vargas <rdv230@nyu.edu>
2016-03-21 20:59:10 -0400

    Added Carl Grand Piano fixed movement.

--- Assets/TestingArc.cs
+++ Assets/TestingArc.cs
@@ -17,12 +17,14 @@ public class TestingArc : MonoBehaviour {
 	public string chordText;
 	public string cText;
 	public GameObject minorTextObj;
+	Vector3 dir;
 
 	bool justPlayed;
 	float time2PlayAgain;
 
 	// Use this for initialization
 	void Start () {
+		dir = Vector3.zero;
 		hasTouched = false;
 		hasSwiped = false;
 		playSwipe = false;
@@ -65,30 +67,34 @@ public class TestingArc : MonoBehaviour {
 			playSwipe = true;
 		}
 			
-
-		Vector3 dir = Vector3.zero;
-		dir.x = Input.acceleration.x;
-		dir.z = Input.acceleration.z;
-
+//
+//		dir = Vector3.zero;
+//		dir.x = Input.acceleration.x;
+//		dir.z = Input.acceleration.z;
+//
 		if(!justPlayed){
-
-		if(dir.x < 0 &&dir.sqrMagnitude> 0.4f){
-			Debug.Log("dir.x: "+ dir.x);
-
-
-			float soundLevel = Mathf.Clamp01(Mathf.Abs(dir.x));
-			Debug.Log("SoundLevel: "+soundLevel);
-//		if(hasTouched||playSwipe){
-//			if(Input.GetTouch(0).position.x <= 89){
-			PlaySound(MajorChords, soundLevel);
-			justPlayed = true;
-				}
+			dir = Vector3.zero;
+			dir.x = Input.acceleration.x;
+			dir.z = Input.acceleration.z;
+			//If I keep the dual check of x and z axis it might sounds a little off because not everytime you move you hit a key
+			//If I remove the z axis one it plays forever.
+			if(dir.x < 0f && dir.z < -1.3f && dir.sqrMagnitude> 0.8f){
+//				|| dir.x < -1.3f && dir.z < 0f && dir.sqrMagnitude >0.8f){
+				Debug.Log("dir: "+ dir);
+				float soundLevel = Mathf.Clamp01(Mathf.Abs(dir.z * 0.2f));
+				Debug.Log("SoundLevel: "+soundLevel);
+				PlaySound(MajorChords, soundLevel);
+				dir = Vector3.zero;
+				justPlayed = true;
+			}
 		}
 
 		if(justPlayed){
+			dir = Vector3.zero;
 			time2PlayAgain+= Time.deltaTime;
 
 			if(time2PlayAgain >= 0.1f){
+				dir = Vector3.zero;
 				justPlayed = false;
 				time2PlayAgain = 0;
 			}
@@ -103,53 +109,58 @@ public class TestingArc : MonoBehaviour {
 
 	void PlaySound(AudioClip[] currentChords,float volumeScale){
 
-		if(transform.rotation.z <= 1 && transform.rotation.z > 0.715){
+		if(transform.rotation.z <= 1 && transform.rotation.z > 0.75){
 			myAudio.PlayOneShot(currentChords[0],volumeScale);
 
-		}else if(transform.rotation.z <= 0.715 && transform.rotation.z > 0.43){
+		}else if(transform.rotation.z <= 0.75 && transform.rotation.z > 0.5){
 			myAudio.PlayOneShot(currentChords[1],volumeScale);
 
-		}else if(transform.rotation.z <= 0.43 && transform.rotation.z > 0.145){ 
+		}else if(transform.rotation.z <= 0.5 && transform.rotation.z > 0.25){ 
 			myAudio.PlayOneShot(currentChords[2],volumeScale);
 
-		}else if(transform.rotation.z <= 0.145 && transform.rotation.z > -0.14){
+		}else if(transform.rotation.z <= 00.25 && transform.rotation.z > 0){
 			myAudio.PlayOneShot(currentChords[3],volumeScale);
 
-		}else if(transform.rotation.z <= -0.14 && transform.rotation.z > -0.425){
+		}else if(transform.rotation.z <= 0 && transform.rotation.z > -0.25){
 			myAudio.PlayOneShot(currentChords[4],volumeScale);
 
-		}else if(transform.rotation.z <= -0.425 && transform.rotation.z > -0.71){
+		}else if(transform.rotation.z <= -0.25 && transform.rotation.z > -0.5){
 			myAudio.PlayOneShot(currentChords[5],volumeScale);
 
-		}else if(transform.rotation.z <= -0.71 && transform.rotation.z > -1){
+		}else if(transform.rotation.z <= -0.5 && transform.rotation.z > -0.75){
 			myAudio.PlayOneShot(currentChords[6],volumeScale);
+		}else if(transform.rotation.z <= -0.75 && transform.rotation.z > -1){
+			myAudio.PlayOneShot(currentChords[7],volumeScale);
 		}
+	
 		playSwipe = false;
 	}
 
 	string ChangeChordText(){
 
-			if(transform.rotation.z <= 1 && transform.rotation.z > 0.715){
-				cText = "A";
+		if(transform.rotation.z <= 1 && transform.rotation.z > 0.75){
+			cText = "A";
 
-			}else if(transform.rotation.z <= 0.715 && transform.rotation.z > 0.43){
-				cText = "B";
+		}else if(transform.rotation.z <= 0.75 && transform.rotation.z > 0.5){
+			cText = "B";
 
-			}else if(transform.rotation.z <= 0.43 && transform.rotation.z > 0.145){ 
-				cText = "C";
+		}else if(transform.rotation.z <= 0.5 && transform.rotation.z > 0.25){ 
+			cText = "C";
 
-			}else if(transform.rotation.z <= 0.145 && transform.rotation.z > -0.14){
-				cText = "D";
+		}else if(transform.rotation.z <= 0.25 && transform.rotation.z > 0){
+			cText = "D";
 
-			}else if(transform.rotation.z <= -0.14 && transform.rotation.z > -0.425){
-				cText = "E";
+		}else if(transform.rotation.z <= 0 && transform.rotation.z > -0.25){
+			cText = "E";
 
-			}else if(transform.rotation.z <= -0.425 && transform.rotation.z > -0.71){
-				cText = "F";
+		}else if(transform.rotation.z <= -0.25 && transform.rotation.z > -0.5){
+			cText = "F";
 
-			}else if(transform.rotation.z <= -0.71 && transform.rotation.z >= -1){
-				cText = "G";
-			}
+		}else if(transform.rotation.z <= -0.5 && transform.rotation.z >= -0.75){
+			cText = "G";
+		}else if(transform.rotation.z <= -0.75 && transform.rotation.z >= -1){
+			cText = "C";
+		}
 		return cText;
 	}
 
