07fdebddd49d5d142d3326ec820aa5f0bb45f1dc
Reynaldo Vargas <rdv230@nyu.edu>
2016-04-03 17:17:00 -0400

    I'm going to commit this test scene, good to make sure we know what X, Y , Z rotation are in Gyro

---
+++ Assets/rotationScr.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotationScr : MonoBehaviour {
+
+	Gyroscope gyro;
+
+	// Use this for initialization
+	void Start () {
+		gyro = Input.gyro;
+		if(!gyro.enabled)
+		{
+			gyro.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.transform.rotation  = gyro.attitude;
+
+
+	
+	}
+}
